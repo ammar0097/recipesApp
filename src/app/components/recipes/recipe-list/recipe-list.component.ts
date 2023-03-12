6102bce67a67b57b49136c02ca46afe78f93fe45
Ammar <ammarferchichi7@gmail.com>
2023-03-12 11:57:20 +0100

    more html featrues

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -4,15 +4,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
-  recipes : Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'spaghetti',
+      'the best italian food',
+      'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg'
+    ),
+    new Recipe(
+      'spaghetti',
+      'the best italian food',
+      'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg'
+    ),
+    new Recipe(
+      'spaghetti',
+      'the best italian food',
+      'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg'
+    )
+  ];
 
   constructor() {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+ 
+  ingredients: Ingredients[] = [
+    new Ingredients("apples" , 5),
+    new Ingredients("lemon" , 10),
 
+  ];
+ 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
